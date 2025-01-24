4a5a40934ab609324f8b241237d581580d748307
Amir Albuquerque <amirsilva00an@gmail.com>
2025-01-24 16:59:36 -0300

    Adicionar a funcionalidade pressione Enter no .HTML e a função adicionar amigo no .JS

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let amigos = [];
+//let i = 0;
+
+function limparCampo(){
+    document.querySelector("input").value = "";
+}
+
+function adicionarAmigo(){
+    if (document.querySelector("input").value == ""){
+        alert("Por favor, digite o nome do(a) amigo(a)");
+    }
+    else {
+        //amigos[i] = document.querySelector("input").value;
+        amigos.push(document.querySelector("input").value);
+        limparCampo();
+        //i++;
+        console.log(amigos);
+    }
+}
