fcafd9039873ac2cd9be7aaaaf897e26e5ce68cf
Amir Albuquerque <amirsilva00an@gmail.com>
2025-01-27 18:20:19 -0300

    Implementar o sorteador do amigo secreto

--- app.js
+++ app.js
@@ -24,10 +24,23 @@ function adicionarAmigo(){
         listaAmigos.innerHTML = "";
         // Estrutura de repetição
         amigos.forEach((amigo) => {
-        let listaItem = document.createElement("li"); //Cria um item <li>
+        let listaItem = document.createElement("li"); //Cria um item <"li">
         listaItem.textContent = amigo; //Define o texto do item como nome de cada amigo
         listaAmigos.appendChild(listaItem); //Adiciona o item a <ul>
         console.log(listaAmigos);
         });
+    }
+}
+
+function sortearAmigo(){
+    if (amigos.length != 0){   
+        let resultado = document.getElementById("resultado");
+        //Gera um índice aleátorio para o array amigos
+        let amigoSorteado = amigos[parseInt(Math.random() * amigos.length)];
+        console.log(amigoSorteado);
+        resultado.innerHTML = amigoSorteado;
+    }
+    else{
+        alert("Favor adcionar pelo menos 3 amigos!")
     }
 }
