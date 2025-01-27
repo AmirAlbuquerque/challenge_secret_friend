943d8337006f320b360435d9a34cf21dcea33c17
Amir Albuquerque <amirsilva00an@gmail.com>
2025-01-27 17:49:45 -0300

    Implementar a lista de amigos ao clicar no botão adicionar

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ function limparCampo(){
     document.querySelector("input").value = "";
 }
 
+// Função para armazenar o nome dos amigos e limpar o input
 function adicionarAmigo(){
     if (document.querySelector("input").value == ""){
         alert("Por favor, digite o nome do(a) amigo(a)");
@@ -17,5 +18,16 @@ function adicionarAmigo(){
         limparCampo();
         //i++;
         console.log(amigos);
+        
+        // Adicionando os amigos em forma de lista
+        let listaAmigos = document.getElementById("listaAmigos");
+        listaAmigos.innerHTML = "";
+        // Estrutura de repetição
+        amigos.forEach((amigo) => {
+        let listaItem = document.createElement("li"); //Cria um item <li>
+        listaItem.textContent = amigo; //Define o texto do item como nome de cada amigo
+        listaAmigos.appendChild(listaItem); //Adiciona o item a <ul>
+        console.log(listaAmigos);
+        });
     }
 }
