235fe7fc3595f1c562da0a6b4174fd34ef2d6566
Amir Albuquerque <amirsilva00an@gmail.com>
2025-02-01 00:12:58 -0300

    Adicionando .gitignore e incorporando Nde.JS

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-//Definindo o dicionário amigos.
-let amigos = {};
-
+let amigos = {}; //Definindo o dicionário amigos.
+let listaSorteio = [] // Define lista para realizar sorteio.
 //Função para sempre limpar os campos de input
 function limparCampo(){
     document.getElementById("nome").value = "";
@@ -11,10 +10,11 @@ function limparCampo(){
 function adicionarAmigo(){
     let nome = document.getElementById("nome").value // Definindo a variável nome como o input inserido no campo "nome"
     let email = document.getElementById("email").value // Definindo a variável email como o input inserido no campo "email"
+    listaSorteio.push(nome); // Adiciona lista para sorteio
+    console.log(listaSorteio);
     // Adicionando os amigos em forma de lista
     let listaAmigos = document.getElementById("listaAmigos");
     listaAmigos.innerHTML = ""; // Limpando a lista de amigos do HTML
-
     //Verificando se o campo nome e e-mail não estão vazios antes de prosseguir
     if (nome == ""){
         alert("Por favor, digite o nome do(a) amigo(a)");
@@ -30,22 +30,65 @@ function adicionarAmigo(){
             let listaItem = document.createElement("li"); //Cria um item <"li">
             listaItem.textContent = `${chave}: ${amigos[chave]}`; //Define o texto do item como nome e e-mail de cada amigo
             listaAmigos.appendChild(listaItem); //Adiciona o item a <ul>
-            console.log(listaAmigos);
         }
     }
 }
 
-//Função para exibir o resultado
-function sortearAmigo(){
-    if (amigos.length != 0){   
-        let resultado = document.getElementById("resultado");
-        //Gera um índice aleátorio para o array amigos
-        let amigoSorteado = amigos[parseInt(Math.random() * amigos.length)];
-        console.log(amigoSorteado);
-        resultado.innerHTML = amigoSorteado;
 
-    }
-    else{
-        alert("Favor adcionar pelo menos 3 amigos!")
+// Muda a ordem da lista de amigos
+function embaralharAmigos(lista) {
+    for (let i = lista.length -1 ; i >0; i--) {
+        const j = parseInt(Math.random() * (i+1));
+        [lista[i], lista[j]] = [lista[j], lista[i]];
     }
 }
+
+//Função para exibir o resultado
+function sortearAmigo(amigos){
+    if (listaSorteio.length < 2){  
+        alert("Favor adcionar pelo menos 3 amigos!");
+        return null;
+    }
+    /*else{
+        let resultado = document.getElementById("resultado");
+        //Gera um índice aleátorio para o array amigos
+        let amigoSorteado = listaSorteio[parseInt(Math.random() * listaSorteio.length)];
+        console.log(amigoSorteado);
+        resultado.innerHTML = amigoSorteado;
+    }*/
+    const dicSorteio = {};
+    let tentativa = 0;
+    
+    do{
+        tentativa++;
+        embaralharAmigos(listaSorteio);
+        //dicSorteio = {};
+
+        let verificacao = true;
+        for(let i = 0; i < listaSorteio.length; i++) {
+            //const primeiroAmigo = listaSorteio[i]; // Pega o primeiro amigo da lista
+            const sorteado = listaSorteio[(i+1) % listaSorteio.length]; // Pega o último amigo da lista
+            if (listaSorteio[i] === sorteado) {
+                verificacao = false;
+                break;
+            }
+            dicSorteio[listaSorteio[i]] = sorteado;
+        }
+        if (verificacao) break;
+    } while(tentativa < 100);
+
+    if (tentativa >= 100) {
+        alert("Não foi possível realizar o sorteio corretamente.");
+        return null;
+    }
+
+    console.log(`Sorteio realizado após ${tentativa} tentativas.`);
+    console.log("Resultado do sorteio:", dicSorteio);
+    return dicSorteio;
+}
+
+/*console.log(dicSorteio);
+    for(let i=0; i<listaSorteio.length; i++){
+        const nome = listaSorteio[i];
+        console.log(amigos[dicSorteio[nome]])
+    }*/
